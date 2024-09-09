e1b5587739c0c8a4368fa3a256edbfb5d40b093e
hasansisik <gegify@gmail.com>
2024-09-09 19:10:57 +0300

    Refactor: Remove console logs and update push notification handling in sendNotification helper

--- helpers/sendNotification.js
+++ helpers/sendNotification.js
@@ -2,12 +2,18 @@ const axios = require("axios");
 
 const sendNotification = async (expoPushToken, message) => {
   try {
+    console.log("Bildirim gönderiliyor...");
+    console.log("Expo Push Token:", expoPushToken);
+    console.log("Mesaj:", message);
+
     const response = await axios.post("https://exp.host/--/api/v2/push/send", {
       to: expoPushToken,
       sound: "default",
       title: "Yeni Görev",
       body: message,
     });
+
+    console.log("Bildirim başarıyla gönderildi:", response.data);
   } catch (error) {
     console.error(
       "Bildirim gönderilemedi:",
