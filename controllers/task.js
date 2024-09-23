510d7994715d1de761bda29bb649301f92252932
hasansisik <gegify@gmail.com>
2024-09-23 20:36:57 +0300

    Refactor: Remove push notification handling in createTask controller

--- controllers/task.js
+++ controllers/task.js
@@ -45,27 +45,6 @@ const createTask = async (req, res) => {
       .populate("taskCreator")
       .populate("persons")
       .populate("plan");
-    // Notification Send
-    const message = `Yeni görev oluşturuldu: ${taskTitle}`;
-    const personIds = Array.isArray(persons) ? persons : [persons];
-
-    if (personIds.length > 0) {
-      personIds.forEach(async (personId) => {
-        const user = await User.findById(personId);
-        console.log(user);
-        if (user && user.expoPushToken) {
-          await sendFCMNotification(user.expoPushToken, message);
-        } else {
-          console.log(
-            `Kullanıcıya bildirim gönderilemedi. Expo Push Token bulunamadı: ${personId}`
-          );
-        }
-      });
-    } else {
-      console.log(
-        "Kullanıcıya bildirim gönderilemedi. Expo Push Token bulunamadı."
-      );
-    }
 
     res.status(StatusCodes.CREATED).json(tasks);
   } catch (error) {
--- helpers/sendFCMNotification.js
+++ helpers/sendFCMNotification.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 
-const sendFCMNotification = async (expoPushToken, message) => {
+const sendNotification = async (expoPushToken, message) => {
   const payload = {
     to: expoPushToken,
     sound: "default",
@@ -9,11 +9,17 @@ const sendFCMNotification = async (expoPushToken, message) => {
   };
 
   try {
-    const response = await axios.post("https://exp.host/--/api/v2/push/send", payload);
+    const response = await axios.post(
+      "https://exp.host/--/api/v2/push/send",
+      payload
+    );
     console.log("Bildirim başarıyla gönderildi:", response.data);
   } catch (error) {
-    console.error("Bildirim gönderilemedi:", error.response ? error.response.data : error.message);
+    console.error(
+      "Bildirim gönderilemedi:",
+      error.response ? error.response.data : error.message
+    );
   }
 };
 
-module.exports = sendFCMNotification;
+module.exports = sendNotification;
